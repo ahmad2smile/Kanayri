79c40517200dd16d20a39cd4a1dca6e6d97d159b
ahmad <ahmad2smile@protonmail.com>
2020-02-02 23:08:11 +0500

    Add Product Aggregate Test

---
+++ Kanayri.Tests/Domain/Product/ProductAggregateTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Kanayri.Domain.Product.Events;
+using Xunit;
+
+namespace Kanayri.Tests.Domain.Product
+{
+    public class ProductAggregateTest
+    {
+        [Fact]
+        public void HandleCreatedProductEventOnAgg()
+        {
+            var id = Guid.NewGuid();
+            const string name = "Test Product Name";
+            const int price = 400;
+
+            var productCreatedEvent = new ProductCreatedEvent(id, name, price);
+
+            var agg = new Kanayri.Domain.Product.Product();
+
+            agg.Handle(productCreatedEvent);
+
+            Assert.Equal(id, agg.Id);
+            Assert.Equal(name, agg.Name);
+            Assert.Equal(price, agg.Price);
+        }
+    }
+}
